373f8ce3014faacef6089b465cc286ff7f88252c
adarsh kumar khosla <khoslaadarsh@gmail.com>
2024-05-26 12:15:38 +0530

    first commit

---
+++ components/AuthProviders.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AuthProviders = () => {
+  return (
+    <div>
+      <h1>
+        Hello AuthProviders
+      </h1>
+    </div>
+  )
+}
+
+export default AuthProviders
